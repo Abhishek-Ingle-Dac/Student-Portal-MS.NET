1037bb5e80f7e21f58796d80b8647f0443e562f9
Abhishek <abhishekingle5637@gmail.com>
2025-05-29 19:46:14 +0530

    Student Portal Project Uploaded

---
+++ StudentPoral/StudentPoral.Web/Data/DbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using System;
+using System.IO;
+
+namespace StudentPoral.Web.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("StudentPortal");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 34)));
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
